7e72e0bedad83ef48a64c0c559cdc2030e4ee2e5
i25959341 <karlson.lee09@gmail.com>
2017-01-15 21:10:58 +0000

    back to square one

--- generate.py
+++ generate.py
@@ -87,19 +87,19 @@ def splitData(data, steerings,pct):
 
 def generate(filepath, pct, batchSize=32,flip=False):
     data, steerings = read(filepath, folderName="data/")
-    data1, steerings1 = read("rightTurn/driving_log.csv", folderName="rightTurn/")
-    data+= data1
-    steerings+=steerings1
-    data2, steerings2 = read("dirtTurn/driving_log.csv", folderName="dirtTurn/")
-    data+= data2
-    steerings+=steerings2
-    data3, steerings3 = read("dirtRoad/driving_log.csv", folderName="dirtRoad/")
-    data+= data3
-    steerings+=steerings3
-
-    data4, steerings4 = read("right/driving_log.csv", folderName="right/")
-    data+= data4
-    steerings+=steerings4
+    # data1, steerings1 = read("rightTurn/driving_log.csv", folderName="rightTurn/")
+    # data+= data1
+    # steerings+=steerings1
+    # data2, steerings2 = read("dirtTurn/driving_log.csv", folderName="dirtTurn/")
+    # data+= data2
+    # steerings+=steerings2
+    # data3, steerings3 = read("dirtRoad/driving_log.csv", folderName="dirtRoad/")
+    # data+= data3
+    # steerings+=steerings3
+    #
+    # data4, steerings4 = read("right/driving_log.csv", folderName="right/")
+    # data+= data4
+    # steerings+=steerings4
 
     data, steerings= shuffle(data,steerings)
     train, yTrain, valid, yValid = splitData(data, steerings, pct)
